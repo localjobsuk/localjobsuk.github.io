3f35499af2dd8a4ac9b7ed494b0f5df0835f3aaf
localjobsuk <DM1Pro@icloud.com>
2026-02-26 14:08:01 +0000

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,7 +1,25 @@
-import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-const auth = getAuth();
+// Use compat version (same as your other pages)
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT.firebaseapp.com",
+    projectId: "YOUR_PROJECT",
+    storageBucket: "YOUR_PROJECT.appspot.com",
+    appId: "YOUR_APP_ID"
+  });
+}
 
-window.logout = async function () {
-  await signOut(auth);
-  window.location.href = "login.html";
-};
+const auth = firebase.auth();
+
+document.addEventListener("DOMContentLoaded", function () {
+  const logoutBtn = document.getElementById("logoutBtn");
+
+  if (logoutBtn) {
+    logoutBtn.addEventListener("click", function (e) {
+      e.preventDefault();
+      auth.signOut().then(() => {
+        window.location.href = "login.html";
+      });
+    });
+  }
+});
